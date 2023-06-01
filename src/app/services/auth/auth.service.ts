8b93c16db53b3538f2f74c93eea0b2fc8bd28c56
unknowncode44 <matiaz.orellana@gmail.com>
2023-06-01 08:46:14 -0300

    230601

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable   } from '@angular/core';
+import { AuthModule   } from 'src/app/modules/auth/auth.module';
+import { HttpClient   } from '@angular/common/http'
+import { Observable   } from 'rxjs';
+import { Credentials  } from '../../shared/models/credentials.model';
+import { User         } from '../../shared/models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: AuthModule
 })
 export class AuthService {
 
-  constructor() { }
+  apiUrl: string = ''
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(credentials: Credentials): Observable<any> {
+    return this.http.post(this.apiUrl, credentials)
+  }
+
+  register(user: User): Observable<User>{
+    return this.http.post<User>(this.apiUrl, user)
+  }
 }
