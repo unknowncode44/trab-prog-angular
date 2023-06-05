8e38b0dd12ff1729f5d86bb2041bfcf4c2d9758a
MatyMoya15 <matymoyano4@gmail.com>
2023-06-05 04:00:30 -0300

    230605 login

--- src/app/components/auth/login/login/login.component.ts
+++ src/app/components/auth/login/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AbstractControl, FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Credentials } from 'src/app/shared/models/credentials.model';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +12,39 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  
+  loginForm!: FormGroup;
 
-  onSubmit(){}
+  loading: boolean = false;
+
+  constructor(
+    private auth : AuthService, private router: Router, private formsBuilder : FormBuilder){}
+
+  ngOnInit(): void{
+    this.loginForm = this.formsBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  onSubmit(){
+    let credentials: Credentials = {
+      username: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    }
+    this.login(credentials)
+  }
+
+  private login(credentials : Credentials){
+    this.loading = true
+    this.auth.login(credentials).subscribe({
+      next: (user)=>{
+        this.router.navigate(['tareas'])
+      },
+      error: (error) =>{
+        console.log(error)
+        this.loading = false
+      }
+    })
+  }
 
 }
